7d5490cbf7840782acfb36b44027680934c83e8b
Mike Straw <mike.straw@automattic.com>
2024-12-02 15:10:50 -0500

    Adding initial interactivity

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8b119b91c2b270cb7972');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b00a4f820d111fd9c17c');
