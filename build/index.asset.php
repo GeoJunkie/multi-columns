f440941e074fe803a9888cc91881dac4d367342e
Mike Straw <mike.straw@automattic.com>
2024-12-02 15:16:35 -0500

    Adding controls;converting to a div

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b00a4f820d111fd9c17c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5732d7f8d794e1c620f0');
